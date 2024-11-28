ee79221520cbef96d2062d464e5ca22b6516fe9a
sunrise <alba.segurallamas@gmail.com>
2024-11-28 16:59:37 +0100

    recylced view

--- app/src/main/java/com/example/m7_geeco_in/MainActivity.kt
+++ app/src/main/java/com/example/m7_geeco_in/MainActivity.kt
@@ -20,11 +20,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        // Set onClickListener for the button
-        findViewById<Button>(R.id.button).setOnClickListener {
-            val intent = Intent(this, LlistaIngressos::class.java)
-            startActivity(intent)
-        }
     }
 }
