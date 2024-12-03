b7891dafe40c204f12231988fb73e5cb70c9f43e
sunrise <alba.segurallamas@gmail.com>
2024-12-03 20:39:47 +0100

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/m7_geeco_in/LlistaIngressos.kt
+++ app/src/main/java/com/example/m7_geeco_in/LlistaIngressos.kt
@@ -1,24 +1,30 @@
 package com.example.m7_geeco_in
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.content.Intent;
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 
 class LlistaIngressos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_llista_ingressos)
         val recyclerview = findViewById<RecyclerView>(R.id.recycler1)
+        val b1 = findViewById<Button>(R.id.button1)
         recyclerview.layoutManager = LinearLayoutManager(this)
         val data = ArrayList<ItemsView>()
-        for (i in 1..10000) {
+        for (i in 1..20) {
             data.add(ItemsView(R.drawable.money, "Item $i"))
         }
         val adapter = CustomAdapter(data)
         recyclerview.adapter = adapter
+        b1.setOnClickListener({
+            val intent = Intent(this@LlistaIngressos, Filtres::class.java)
+            startActivity(intent)
+        })
     }
 }
