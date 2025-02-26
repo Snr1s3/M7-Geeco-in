5eabebee327864ac6e7cf90a007c909062c58b56
Alba Segura <2023_alba.segura@gmail.com>
2025-02-26 20:38:03 +0100

    Filtres fets: -Titol -Quantitat, Falta dates i controlar majuscules i miniscules

--- app/src/main/java/com/example/m7_geeco_in/LlistaDespeses.kt
+++ app/src/main/java/com/example/m7_geeco_in/LlistaDespeses.kt
@@ -3,23 +3,24 @@ package com.example.m7_geeco_in
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.m7_geeco_in.data.geecoinAPI
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
-data class Despesses (val expense_id:Int, val idUsuari:Int, val noms:String, val imports:Int)
+data class Despesses(val title:String, val desc:String, val amount:Int, val date:String, val id:Int)
 
 class LlistaDespeses : AppCompatActivity() {
 
-    companion object {
-        val Despeses: List<Despesses> = listOf(
-            Despesses(1, 1, "Alba", 200),
-            Despesses(2,2,"Dani", 300),
-            Despesses(3,3,"Karolayn", 400))
-    }
+    private  var expenses: List<Despesses>? = listOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,11 +29,12 @@ class LlistaDespeses : AppCompatActivity() {
         val b2 = findViewById<Button>(R.id.button2)
         val bundle = intent.extras
         if (bundle != null && bundle.size() != 0) {
-            val stringValue = bundle?.getString("fNom") ?: "Null"
-            val stringValue2 = bundle.getString("fDiners") ?: "Null"
-            AddData(Despeses, "Null", "Null")
+            val stringValue = bundle.getString("fNom") ?: null
+            val stringValue2 = bundle.getString("fDiners") ?: null
+            val import = stringValue2?.toIntOrNull() ?: null
+            fetchDespesesList(skip = 0, limit = 10,stringValue, import)
         } else {
-            AddData(Despeses, "Null", "Null")
+            fetchDespesesList(skip = 0, limit = 10,null,null)
         }
 
         b1.setOnClickListener{
@@ -45,18 +47,50 @@ class LlistaDespeses : AppCompatActivity() {
         }
     }
 
-    fun AddData(Despesses:List<Despesses>, key: String, key2: String) {
+    fun AddData(Despesses:List<Despesses>, key: String?, key2: Int?){
         val recyclerview = findViewById<RecyclerView>(R.id.recycler1)
         recyclerview.layoutManager = LinearLayoutManager(this)
-
         val data = ArrayList<ItemsView>()
-        for (despesses in LlistaDespeses.Despeses) {
-            val importsString = despesses.imports.toString()
+        for (despesa in Despesses) {
+            val importsString = despesa.amount.toString()
             val importsString2 = "$importsString€"
-            data.add(ItemsView(R.drawable.money, despesses.noms, importsString2))
+            if(key == null && key2 == null){
+                data.add(ItemsView(R.drawable.money, despesa.title, importsString2))
+            }
+            if(key2 != null && key == null){
+                if(despesa.amount == key2){
+                    data.add(ItemsView(R.drawable.money, despesa.title, importsString2))
+                }
+            }
+            if(key != null && key2 == null){
+                if(despesa.title == key){
+                    data.add(ItemsView(R.drawable.money, despesa.title, importsString2))
+                }
+            }
+            else{
+                if(key == despesa.title && key2 == despesa.amount ){
+                    data.add(ItemsView(R.drawable.money, despesa.title, importsString2))
+                }
+            }
         }
-
         val adapter = CustomAdapter(this, data)
         recyclerview.adapter = adapter
+    }
+
+    private fun fetchDespesesList(skip: Int, limit: Int, key: String?, key2: Int?) {
+        lifecycleScope.launch {
+            try {
+                expenses = geecoinAPI.API().getDespesesList(skip = skip, limit = limit)
+                AddData(expenses!!, key, key2)
+                Toast.makeText(this@LlistaDespeses, "Data loaded successfully!", Toast.LENGTH_SHORT).show()
+            }  catch (e: HttpException) {
+                println("HTTP Error: ${e.message}")
+            } catch (e: IOException) {
+                println("Network Error: ${e.message}")
+            } catch (e: Exception) {
+                Toast.makeText(this@LlistaDespeses,"Error loading data", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@LlistaDespeses, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/m7_geeco_in/LlistaIngressos.kt
+++ app/src/main/java/com/example/m7_geeco_in/LlistaIngressos.kt
@@ -1,27 +1,25 @@
 package com.example.m7_geeco_in
 
 import android.os.Bundle
-import android.content.Intent;
+import android.content.Intent
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.m7_geeco_in.data.geecoinAPI
+
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
 // import com.example.recyclerview.reserves.ReservesAPI
 
-data class Ingressos(val income_id:Int, val idUsuari:Int, val noms:String, val imports:Int)
+data class Ingressos(val title:String, val desc:String, val amount:Int, val date:String, val id:Int)
 
 class LlistaIngressos : AppCompatActivity() {
-    companion object {
-        val Ingressos:List<Ingressos> = listOf(
-            Ingressos(1, 2, "Alba", 1000),
-            Ingressos(2, 2, "Dani", 2000),
-            Ingressos(3, 4, "Karolayn", 3000))
-    }
-
+    private  var incomes: List<Ingressos>? = listOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_llista_ingressos)
@@ -29,15 +27,14 @@ class LlistaIngressos : AppCompatActivity() {
         val b2 = findViewById<Button>(R.id.button2)
         val bundle = intent.extras
        if (bundle != null && bundle.size() != 0) {
-            val stringValue = bundle?.getString("fNom") ?: "Null"
-            val stringValue2 = bundle.getString("fDiners") ?: "Null"
-            val import = stringValue2.toIntOrNull() ?: 0
-            AddData(Ingressos,"Null", "Null")
+           val stringValue = bundle.getString("fNom") ?: null
+           val stringValue2 = bundle.getString("fDiners") ?: null
+           val import = stringValue2?.toIntOrNull() ?: null
+           fetchIngressosList(skip = 0, limit = 10,stringValue, import)
         } else {
-            AddData(Ingressos, "Null", "Null")
+           Toast.makeText(this@LlistaIngressos, "Aqui", Toast.LENGTH_SHORT).show()
+           fetchIngressosList(skip = 0, limit = 10,null, null)
         }
-
-
         b1.setOnClickListener{
             val intent = Intent(this@LlistaIngressos, Filtres::class.java)
             startActivity(intent)
@@ -48,19 +45,53 @@ class LlistaIngressos : AppCompatActivity() {
         }
     }
 
-    fun AddData(Ingressos:List<Ingressos>, key: String, key2: String){
+    fun AddData(ingressos:List<Ingressos>, key: String?, key2: Int?){
         val recyclerview = findViewById<RecyclerView>(R.id.recycler1)
         recyclerview.layoutManager = LinearLayoutManager(this)
 
         val data = ArrayList<ItemsView>()
-        for (ingres in LlistaIngressos.Ingressos) {
-            val importsString = ingres.imports.toString()
+        for (ingres in ingressos) {
+            val importsString = ingres.amount.toString()
             val importsString2 = "$importsString€"
-            data.add(ItemsView(R.drawable.money, ingres.noms, importsString2))
+            if(key == null && key2 == null){
+                data.add(ItemsView(R.drawable.money, ingres.title, importsString2))
+            }
+            if(key2 != null && key == null){
+                if(ingres.amount == key2){
+                    data.add(ItemsView(R.drawable.money, ingres.title, importsString2))
+                }
+            }
+            if(key != null && key2 == null){
+                if(ingres.title == key){
+                    data.add(ItemsView(R.drawable.money, ingres.title, importsString2))
+                }
+            }
+            else{
+                if(key == ingres.title && key2 == ingres.amount ){
+                    data.add(ItemsView(R.drawable.money, ingres.title, importsString2))
+                }
+            }
         }
 
         val adapter = CustomAdapter(this, data)
         recyclerview.adapter = adapter
+    }
+    private fun fetchIngressosList(skip: Int, limit: Int, key: String?, key2: Int?) {
+        lifecycleScope.launch {
+            try {
+                incomes = geecoinAPI.API().getIngressosList(skip = skip, limit = limit)
+                AddData(incomes!!, key, key2)
+                Toast.makeText(this@LlistaIngressos, "Data loaded successfully!", Toast.LENGTH_SHORT).show()
+            }  catch (e: HttpException) {
+                println("HTTP Error: ${e.message}")
+            } catch (e: IOException) {
+                println("Network Error: ${e.message}")
+            } catch (e: Exception) {
+                Toast.makeText(this@LlistaIngressos, "Error loading data", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@LlistaIngressos, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+
+            }
+        }
     }
 
 }
