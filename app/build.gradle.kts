fed50087491340db42925f71fef43ea08feca87c
Karolayn Muñoz <karolayn.mruiz@gmail.com>
2025-03-27 16:45:07 +0100

    Dependències i llibreries afegides per la nova act.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -33,15 +34,22 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
-    // retrofit
     implementation(libs.retrofit)
-    // gson converter
     implementation (libs.converter.gson)
-    //Parcelize
     implementation( libs.kotlin.parcelize.runtime)
+    implementation(libs.firebase.firestore.ktx)
+    implementation(libs.androidx.datastore.core.android)
+    implementation(platform( libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.firestore)
+    implementation(libs.mpandroidchart)
+    androidTestImplementation(libs.junit.jupiter)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
